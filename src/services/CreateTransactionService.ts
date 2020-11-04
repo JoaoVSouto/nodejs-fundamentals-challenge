d61c0c1e325b3ce6b717fa8008aa90bff601047b
JoaoVSouto <joaovitorss99@hotmail.com>
2020-11-04 11:19:34 -0300

    Validates outcome value based on current balance

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -15,6 +15,14 @@ class CreateTransactionService {
   }
 
   public execute({ title, type, value }: Request): Transaction {
+    if (type === 'outcome') {
+      const { total } = this.transactionsRepository.getBalance();
+
+      if (total < value) {
+        throw Error("You don't have enough balance to buy this product.");
+      }
+    }
+
     const transaction = this.transactionsRepository.create({
       title,
       type,
